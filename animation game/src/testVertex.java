368df89e623e2a8cb1e302193947351d594eb854
Ruf.Jochen <Ruf.Jochen@SK-448A5BCB8A89>
2015-10-02 08:14:28 +0200

    Bischof der Zweite

---
+++ animation game/src/testVertex.java
@@ -0,0 +1,15 @@
+
+public class testVertex {
+	public static void main(String[] args) {
+	/*	Vertex v1 = new Vertex(42,53);
+		Vertex v2 = new Vertex(32,4);
+		System.out.println(v1.distance(v2));
+		v2.normalize();
+		System.out.println(v1.toString());
+		System.out.println(v1.length());  */
+		
+		
+		GeometricObject o1 = new GeometricObject(27, 2);
+		System.out.println(o1);
+	}
+}
